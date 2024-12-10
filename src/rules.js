b040a67485496f108aa2e517b8fe6cf5a3322781
Habid Enrique Manzur Restrepo <17788438+hmanzur@users.noreply.github.com>
2024-12-10 08:44:17 -0500

    v1.0.2 (#1)
    * feat(rules): agregados import restrictivos para moment, jquery, jest entre otros. También se agregaron reglas para revenir import relativos en lugar de los alias
    
    * fix(rules): arreglo de reglas y patrones fallandio en version 1.0.1

--- src/rules.js
+++ src/rules.js
@@ -4,6 +4,7 @@ export default {
   "prefer-const": "error", //Recomendar usar const donde sea posible
   "eqeqeq": "error", //Usar siempre === en lugar de ==
   "strict": ["error", "global"], //Aplicar modo estricto en el entorno global
+  "no-unused-vars": ["error", { "vars": "all", "args": "after-used", "ignoreRestSiblings": false }],
   "jsdoc/require-example": [
     "error",
     {
@@ -28,5 +29,36 @@ export default {
   "jsdoc/require-returns-description": "warn", //Descripcion para el retorno
   "jsdoc/require-returns-type": "error", //Tipo requerido para el retorno
   "jsdoc/require-jsdoc": "off",
-  "jsdoc/require-example": "off"
+  "jsdoc/require-example": "off",
+  "no-restricted-imports": [
+    "error",
+    {
+      "paths": [
+        {
+          "name": "moment",
+          "message": "Moment.js está deprecado. Usa alternativas como date-fns o dayjs."
+        },
+        {
+          "name": "jquery",
+          "message": "jQuery ya no se recomienda. Usa vanilla JavaScript o frameworks modernos como React o Vue."
+        },
+        {
+          "name": "jest",
+          "message": "En este proyecto usamos Node.js test runner o Vitest. Evita usar Jest."
+        },
+        {
+          "name": "dotenv",
+          "message": "Evita usar dotenv. Usa variables de entorno nativas de Node.js mediante process.env y configura tus entornos adecuadamente."
+        },
+        {
+          "name": "nodemon",
+          "message": "Evita usar nodemon. Usa el flag `--watch` con Node.js nativo o herramientas modernas como PM2."
+        }
+      ],
+      "patterns": [{
+        "group": ["../*"],
+        "message": "Evita usar rutas relativas como '../'. Usa los alias definidos en el proyecto para mejorar la claridad y consistencia."
+      }]
+    }
+  ]
 };
