0499529072e6994b477559a3dbe6128ed751e954
Habid Enrique Manzur Restrepo <17788438+hmanzur@users.noreply.github.com>
2025-01-17 09:58:14 -0500

    fix(react-tailwind): arreglos de config

--- config/react-tailwind.js
+++ config/react-tailwind.js
@@ -1,23 +1,15 @@
 import tailwindcss from "eslint-plugin-tailwindcss";
 import readableTailwind from "eslint-plugin-readable-tailwind";
 
-import config from "./react.js";
+import base from "./react.js";
 
-const [react, ...rest] = config;
+const [react, tests] = base;
 
 /** @type {import("eslint").Linter.Config[]} */
 export default [
   {
     ...react,
-    plugins: {
-      tailwindcss,
-      "readable-tailwind": readableTailwind,
-    },
-    rules: {
-      tailwindcss: {
-        callees: ["classNames"],
-      }
-    }
+    ...tailwind.configs["flat/recommended"]
   },
-  ...rest
-];
+  tests
+]
