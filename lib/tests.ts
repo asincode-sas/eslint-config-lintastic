b9938dfbb49b46457cb8c404337cd24f5af21861
Alejandro Molina <alejandro.ytf@gmail.com>
2026-06-01 17:01:26 -0500

    feat: migracion archivos js a ts carpeta src/

---
+++ lib/tests.ts
@@ -0,0 +1,25 @@
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+import { type Linter } from "eslint";
+
+export default [
+  {
+    name: "node:tests",
+    files: ["{test,tests}/**/*.{js,ts,jsx,tsx}"],
+    languageOptions: {
+      globals: {
+        asserts: true,
+      },
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      "no-unused-expressions": "off",
+      "no-undefined": "off",
+      "max-lines-per-function": [
+        "error",
+        { max: 150, skipBlankLines: true, skipComments: true, IIFEs: true },
+      ],
+    },
+  },
+  ...tseslint.configs.recommended,
+] as Linter.Config[];
---
+++ src/plugins.ts
@@ -0,0 +1,7 @@
+import jsdoc from "eslint-plugin-jsdoc";
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
+
+export default {
+  jsdoc,
+  "prettier": eslintPluginPrettierRecommended,
+};
