d6612556c68fe90d73dc33610367ea9cd149e99d
Alejandro Molina <alejandro.ytf@gmail.com>
2026-06-03 11:21:43 -0500

    test: migracion typescript archivos testeados

---
+++ test/integration/react-tailwind.spec.ts
@@ -0,0 +1,30 @@
+import { describe, test, expect } from "vitest";
+
+import { ESLint } from "eslint";
+import { fileURLToPath } from "url";
+
+import baseConfig from "#lib/react-tailwind";
+
+describe("Integration Test -> React Tailwind", () => {
+  test("react-tailwind:base should well integrated", async () => {
+    const eslint = new ESLint({ baseConfig });
+
+    const dir = fileURLToPath(new URL("../samples", import.meta.url));
+    console.log(dir);
+    const [
+      {
+        errorCount,
+        fatalErrorCount,
+        warningCount,
+        fixableErrorCount,
+        fixableWarningCount,
+      },
+    ] = await eslint.lintFiles([`${dir}/**`]);
+
+    expect(errorCount).toBe(12);
+    expect(fatalErrorCount).toBe(0);
+    expect(warningCount).toBe(0);
+    expect(fixableErrorCount).toBe(4);
+    expect(fixableWarningCount).toBe(0);
+  });
+});
--- test/unit/react.spec.ts
+++ test/unit/react.spec.ts
@@ -6,7 +6,6 @@ describe("React", () => {
   test("Sould have well formed config", () => {
     const [_, base, tests] = react;
 
-    console.log(base);
     expect(base).toBeDefined();
     expect(tests).toBeDefined();
     expect(base).toHaveProperty("rules");
@@ -18,7 +17,6 @@ describe("React", () => {
       settings: unknown;
     };
 
-    expect(rules).toHaveProperty("react/jsx-no-useless-fragment");
     expect(rules).toHaveProperty("id-length");
     expect(rules["id-length"]).toBe("off");
     expect(settings).toBeDefined();
