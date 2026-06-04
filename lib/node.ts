232fa1f1d78781ec84be3e067ad4860b048285c4
Alejandro Molina <alejandro.ytf@gmail.com>
2026-06-04 16:13:12 -0500

    fix: imports, alias y tsconfig.build

--- lib/node.ts
+++ lib/node.ts
@@ -1,28 +1,38 @@
 import jsdoc from "eslint-plugin-jsdoc";
 import tseslint from "typescript-eslint";
-
-import { rulesJS, rulesTS } from "#src/rules";
+import { rulesJS } from "#src/rules";
 import plugins from "#src/plugins";
 import ignores from "#src/ignores";
 import tests from "#lib/tests";
 import globals from "globals";
-import type { Linter } from "eslint";
+import { defineConfig } from "eslint/config";
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
 
 const languageOptions = {
   sourceType: "module",
   globals: globals.node,
 };
 
-export default [
+export default defineConfig([
   jsdoc.configs["flat/recommended-error"],
+  eslintPluginPrettierRecommended,
   {
     files: ["**/*.{js,mjs,cjs,ts}"],
     name: "node:base",
     languageOptions,
     plugins,
     rules: {
-      ...rulesJS,
-      ...rulesTS,
+      ...(rulesJS as Record<string, unknown>),
+      "@typescript-eslint/no-empty-function": 'error',
+      "@typescript-eslint/no-explicit-any": 'error',
+      "prettier/prettier": ["error", { endOfLine: "auto" }],
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        {
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+        },
+      ],
       // Node exclusives
       "max-params": ["error", 4],
       "new-cap": ["error", { capIsNew: false }],
@@ -33,4 +43,4 @@ export default [
   {
     ignores,
   },
-] as Linter.Config[];
+])
--- src/plugins.ts
+++ src/plugins.ts
@@ -1,7 +1,5 @@
 import jsdoc from "eslint-plugin-jsdoc";
-import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
 
 export default {
   jsdoc,
-  "prettier": eslintPluginPrettierRecommended,
 };
