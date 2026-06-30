b9cb2092a807d106b0f5e9918ad65b50bc580d12
Alejandro Molina <alejandro.ytf@gmail.com>
2026-06-30 15:08:06 -0500

    fix: added prettier rules in react eslint

--- lib/rules/react-tailwind.ts
+++ lib/rules/react-tailwind.ts
@@ -1,10 +1,12 @@
 import base from "#rules/react.js";
 import { defineConfig } from "eslint/config";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 
 const [jsdoc, react, ...rest] = base;
 
 export default defineConfig([
   jsdoc,
+  eslintPluginPrettierRecommended,
   {
     ...react,
     name: "react:tailwindcss",
